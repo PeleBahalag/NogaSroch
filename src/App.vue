65e4327ffccc638a19dc4b6a9fae7c7038e0f455
Pele <pelebahalag@gmail.com>
2025-08-13 08:54:21 +0300

    first upload

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<script>
+import FactsCard from './components/FactsCard.vue';
+import info from './assets/texts/info.json'
+  export default {
+    data(){
+      return{
+        userTrue : null,
+        info
+      }
+    },
+    components : {FactsCard},
+    mounted(){
+      setTimeout(() => {
+        if(confirm('האם שרוך ורוד זה השרוך הכי יפה בצה"ל??')){
+          this.userTrue = true;
+        }
+        else {
+          this.userTrue = false;
+        }
+      } , 200)
+    }
+  }
+</script>
+
+<template>
+  <div v-if="userTrue || userTrue === null">
+    <h1 class="title">הפרוייקט הראשון שלי בסיירים</h1>
+    <h3 class="subtitle">לומדת שרוך מפ"ל</h3>
+    <img src="./assets/srochim.jpg" alt="sroch" class="sroch">
+    <div class="facts">
+      <facts-card v-for="(fact, index) in info.factsArray" :key="index" :fact="fact"></facts-card>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+body {
+  overflow: hidden;
+  direction: rtl;
+}
+
+.title {
+  color: rgb(255, 0, 170);
+}
+
+.subtitle {
+  color: rgb(182, 0, 121);
+  font-size: 1.8rem;
+}
+
+.sroch {
+  width: 15vw;
+}
+
+.facts {
+  display: flex;
+}
+</style>
---
+++ src/components/FactsCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="facts-card">
+    <p>{{ fact }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props : {
+        fact : String
+    },
+}
+</script>
+
+<style>
+.facts-card{
+    background-color: rgba(118, 57, 72, 1);
+    margin: 2%;
+    border-radius: 25px;
+    padding: 2%;
+    font-size: 1.5rem;
+    display: flex;
+    align-items: center;
+}
+</style>
